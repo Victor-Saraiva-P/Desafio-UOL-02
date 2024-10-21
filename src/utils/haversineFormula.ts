186376ed3b7a78a05676318636976f5e2e9a92c4
Victor Alexandre Saraiva Pimentel <victor_alex2005@hotmail.com>
2024-10-21 16:29:34 -0300

    ✨ feat: Adicionar fórmula de Haversine para cálculo de distâncias

---
+++ src/utils/haversineFormula.ts
@@ -0,0 +1,10 @@
+import haversine from 'haversine';
+
+interface Cordenada {
+  latitude: number;
+  longitude: number;
+}
+
+export const formulaHaversine = (start: Cordenada, end: Cordenada) => {
+  return haversine(start, end);
+};
