9519a3bb839d80fcea85c4f4627c258ab395833f
Victor Alexandre Saraiva Pimentel <victor_alex2005@hotmail.com>
2024-10-21 16:45:09 -0300

    ✨ feat: Adicionar serviço para criação de loja com integração ViaCEP e Google

---
+++ src/services/lojaService.ts
@@ -0,0 +1,38 @@
+import Loja from '../models/lojaModel';
+import { obterEnderecoViaCep } from '../utils/viaCep';
+import { obterCoordenadasViaCep } from '../utils/geocodingGoogle';
+
+interface LojaInput {
+  nome: string;
+  numero: string;
+  segmento: string;
+  cep: string;
+}
+
+export const createStore = async (lojaData: LojaInput) => {
+  const { nome, numero, segmento, cep } = lojaData;
+
+  // Busca o endereço utilizando o ViaCEP
+  const endereco = await obterEnderecoViaCep(cep);
+
+  // Obtém as coordenadas do endereço
+  const coordenadas = await obterCoordenadasViaCep(cep);
+
+  // Cria a nova loja
+  const newLoja = await Loja.create({
+    nome,
+    numero,
+    segmento,
+    cep,
+    logradouro: endereco.logradouro,
+    bairro: endereco.bairro,
+    cidade: endereco.cidade,
+    estado: endereco.estado,
+    latitude: coordenadas.latitude,
+    longitude: coordenadas.longitude,
+  });
+
+  return newLoja;
+};
+
+export default { createStore };
