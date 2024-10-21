4d581c62124f0a6935281786d2bdfaa9a264baaa
Victor Alexandre Saraiva Pimentel <victor_alex2005@hotmail.com>
2024-10-21 17:47:03 -0300

    ✨ feat: Adicionar script para popular o banco de dados com lojas fictícias

--- src/utils/geocodingGoogle.ts
+++ src/utils/geocodingGoogle.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: './config.env' });
 
 interface GoogleGeocodeResponse {
   results: {
